aa82419483837735eee24a9b11a526538ab601e7
Ryan Marshall <rmarshall9n@gmail.com>
2019-02-17 16:54:42 +0000

    Added stub migration class.

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -31,4 +31,30 @@ class SkeletonServiceProvider extends ServiceProvider
     {
         // $this->mergeConfigFrom(__DIR__ . '/../config/skeleton.php', 'skeleton');
     }
+
+    /**
+     * Publish the packages migrations with up to date timestamps.
+     * - Migration files must be stored in package/migrations.
+     * - Migrations must reflect the class name in snake case
+     * - Migrations must use the extension .php.stub.
+     *
+     * @param string|array $migrations
+     * @return void
+     */
+    public function publishMigrations($migrations = [])
+    {
+        $migrations = array_wrap($migrations);
+
+        foreach ($migrations as $className) {
+            if (!class_exists($className)) {
+                $timestamp = date('Y_m_d_His', time());
+                $snakeName = snake_case($className);
+
+                $this->publishes([
+                    __DIR__ . "/../migrations/{$snakeName}.php.stub" => database_path("/migrations/{$timestamp}_{$snakeName}.php"),
+                ], 'migrations');
+            }
+        }
+
+    }
 }
